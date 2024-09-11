08b15af41c6180607468930b38c3720978a025b6
Husjart <Husjart@hotmail.com>
2024-09-11 13:59:21 +0200

    Update main.py
    opsætning af frame

--- main.py
+++ main.py
@@ -4,28 +4,21 @@ from tkinter import messagebox
 def on_button_click():
     messagebox.showinfo("Besked", "Hej")
 
-def tekst1():
-    
-    
-def tekst2():
-    
-    
-def tekst3():
-    
-
 def close_window():
     root.destroy()
 
 # Opret vinduet
 root = tk.Tk()
-root.geometry('300x200-5+40')
+root.geometry('500x500-5+40')
 root.title("Simpelt Tkinter eksempel")
+
+frame = tk.Frame(root, borderwidth=5, relief="ridge", width=100, height=300)
 
 # Opret en knap
 button = tk.Button(root, text="Klik på mig", command=on_button_click)
-knap1 = tk.Button(root, text="1", tekst1)
-knap2 = tk.Button(root, text="2", tekst2)
-knap3 = tk.Button(root, text="3", tekst3)
+knap1 = tk.Button(root, text="1", command=on_button_click)
+knap2 = tk.Button(root, text="2", command=on_button_click)
+knap3 = tk.Button(root, text="3", command=on_button_click)
 btn_close = tk.Button(root, text="Luk vindet", command=close_window)
 
 # Hvor skal knappen være?
@@ -34,6 +27,8 @@ knap1.grid(row=1, column=0, padx=10, pady=5)
 knap2.grid(row=2, column=0, padx=10, pady=5)
 knap3.grid(row=3, column=0, padx=10, pady=5)
 btn_close.grid(row=8, column=1, padx=10, pady=10)  
+
+frame.grid(column=0, row=0, columnspan=1, rowspan=4)
 
 # Start Tkinter event loop
 root.mainloop()
