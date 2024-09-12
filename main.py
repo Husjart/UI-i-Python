cfe2cc9890bc0aa7365e29825d79f1379d8b3ef2
Husjart <Husjart@hotmail.com>
2024-09-12 13:24:59 +0200

    Update main.py
    slet teksten i boxen og efterfølgende skrive det nye.

--- main.py
+++ main.py
@@ -6,7 +6,19 @@ def on_button_click():
 
 def close_window():
     root.destroy()
+
+def tekst1():
+    text_box.delete('1.0', tk.END)
+    text_box.insert('1.0', 'here is my\ntext to insert')
     
+def tekst2():
+    text_box.delete('1.0', tk.END)
+    text_box.insert('1.0', 'mere lort')
+
+def tekst3():
+    text_box.delete('1.0', tk.END)
+    text_box.insert('1.0', 'lort')
+
 # Opret vinduet
 root = tk.Tk()
 root.geometry('500x500-5+40')
@@ -20,9 +32,9 @@ text_box = tk.Text(root, width=40, height=10)
 text_box.grid(row=0, column=1, rowspan=1, padx=10, pady=5)
 # Opret en knap
 button = tk.Button(root, text="Klik på mig", command=on_button_click)
-knap1 = tk.Button(root, text="1", command=on_button_click)
-knap2 = tk.Button(root, text="2", command=on_button_click)
-knap3 = tk.Button(root, text="3", command=on_button_click)
+knap1 = tk.Button(root, text="Indsæt tekst", command=tekst1)
+knap2 = tk.Button(root, text="2", command=tekst2)
+knap3 = tk.Button(root, text="3", command=tekst3)
 btn_close = tk.Button(root, text="Luk vinduet", command=close_window)
 
 # Hvor skal knappen være?
